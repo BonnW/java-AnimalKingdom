3132ab6e08a715210940d6fac6da36275640ab86
bonnw <pwonghansa@gmail.com>
2019-02-06 17:33:52 -0600

    finished Birds class skeleton

---
+++ src/com/lambdaschool/Birds.java
@@ -0,0 +1,35 @@
+package com.lambdaschool;
+
+public class Birds extends AbstractAnimal
+{
+    public String name;
+
+    public Birds(String name, int energy)
+    {
+        super(energy);
+    }
+
+    @Override
+    public void eat()
+    {
+        energy++;
+    }
+
+    @Override
+    public String move()
+    {
+        return "I flap my wings";
+    }
+
+    @Override
+    public String breath()
+    {
+        return "I breath air in my gills";
+    }
+
+    @Override
+    public String reproduce()
+    {
+        return "I just laid some eggs";
+    }
+}
